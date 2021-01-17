0efd76547d5ae858adbe16d1b14c2b43a4b54955
Anastasiya Voroshkevich <the_sun.89@mail.ru>
2021-01-18 02:11:01 +0300

    first commit

---
+++ portfolio/js/main.js
@@ -0,0 +1,174 @@
+$(document).ready(function(){
+
+// Подключение точек пагинации справа page-nav
+$('#page-nav').onePageNav({
+    currentClass: 'active',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+    filter: '',
+    easing: 'swing',
+    begin: function () {},
+    end: function () {},
+    scrollChange: function ($currentListItem) {}
+});
+
+
+const toggleMenu = document.querySelector('.toggle-menu'); //иконка гамбургер
+const mobMenu = document.querySelector('.mobile-menu'); //mob menu
+const overlay = document.querySelector('.overlay'); //overlay
+const bodyEl = document.body;
+
+toggleMenu.addEventListener('click', function(){
+    this.classList.toggle('active');
+    mobMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
+    bodyEl.classList.toggle('noscroll');
+    
+})
+//прослушиваем событие клик моб меню
+mobMenu.addEventListener('click', function(){
+    this.classList.remove('active');
+    toggleMenu.classList.remove('active');
+    overlay.classList.remove('active');
+    bodyEl.classList.remove('noscroll');
+})
+
+
+  // Фильтрация проектов
+  let containerEl = document.querySelector('#portfolio-projects');
+  let mixer = mixitup(containerEl,{
+		  classNames: {
+			  block: ""
+		  }
+  })
+
+// Изменение размеров карточек
+const filterToggles = document.querySelectorAll('.project-menu button');
+const portfolioBigCards = document.querySelectorAll('.project-card');
+
+for (let i = 0; i < filterToggles.length; i++) {
+	filterToggles[i].addEventListener('click', function () {
+		if (i == 0) {
+			for (let j = 0; j < 2; j++) {
+				portfolioBigCards[j].classList.add('big')
+			}
+		 } else {
+			for (let j = 0; j < 2; j++) {
+				portfolioBigCards[j].classList.remove ('big')
+			}
+		}
+	})
+}
+	// form placeholder
+	const formItems = document.querySelectorAll('.form-field');
+	
+	for(let item of formItems){
+		const thisParent = item.closest('.form-item');
+		const thisPlaceholder = thisParent.querySelector('.fake-palceholder');
+		// Если инпут в фокусе		
+		item.addEventListener('focus', function(){
+			thisPlaceholder.classList.add('active');
+		});
+
+		// Если инпут теряет фокус
+		item.addEventListener('blur', function(){
+
+			if(item.value.length > 0){
+				thisPlaceholder.classList.add('active');
+			}
+			else{
+				thisPlaceholder.classList.remove('active');
+			}
+		})
+    }
+    
+	//FORM VALIDATE
+	$('.contact-form').validate({
+		rules: {
+			email: {
+				required: true,
+				email: true
+			},
+			subject: {
+				required: true
+			},
+			message: {
+				required: true
+			}
+		},
+		messages: {
+			email: {
+				required: 'Введите email',
+				email: 'отсутсвует символ @'
+			},
+			subject: {
+				required: 'Введите тему сообщения'
+			},
+			message: {
+				required: 'Введите текст сообщения'
+			}
+		},
+		submitHandler: function (form) {
+			ajaxFormSubmit();
+		}
+	})
+
+// Функция AJAX запрса на сервер
+
+function ajaxFormSubmit() {
+
+	let string = $(".contact-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+	//Формируем ajax запрос
+	$.ajax({
+		type: "POST", // Тип запроса - POST
+		url: "php/mail.php", // Куда отправляем запрос
+		data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+		// Функция если все прошло успешно
+		success: function (html) {
+			$(".contact-form").slideUp(800);
+			$('#answer').html(html);
+		}
+	});
+	// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+	return false;
+}
+	
+
+
+// ПАРАЛЛАКС ДВИЖЕНИЯ ЗА МЫШКОЙ
+let prxScene = document.querySelector('.contacts')
+let prxItem = document.querySelectorAll('.move-quot');
+prxScene.addEventListener('mousemove', function (e) {
+	let x = e.clientX / window.innerWidth;
+	let y = e.clientY / window.innerHeight;
+	for (let item of prxItem) {
+		item.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';}
+	
+});
+
+// показать кнопку скролла вверх
+
+$('#backTop').hide();
+
+$(window).scroll(function(){
+	if($(this).width() < 1200){
+		if( $(this).scrollTop() > 300 ){
+			$('#backTop').fadeIn();
+		}
+		else{
+			$('#backTop').fadeOut();
+		}
+	}
+});
+
+	})
+
+
+
+
+
+
+
